74f216c6abb2c95677c7cf8b54561cba16259a6b
Niels <info@heimseiten.de>
2021-01-06 17:41:10 +0100

    only load scss when timeline is in use

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -9,7 +9,3 @@
  * please view the LICENSE file that was distributed with this source code.
  * @link https://github.com/heimseiten/contao-timeline-bundle
  */
-
-if (TL_MODE == 'FE') {
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaotimeline/timeline.scss|static';
-}
