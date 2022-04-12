0a56ca510568d51b214e008183a49032b8a0eb68
Niels <info@heimseiten.de>
2022-04-12 13:40:29 +0200

    added gallery

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
- * This file is part of Timeline.
- * 
- * (c) Niels Hegmans 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-timeline-bundle
- */
-
-/**
- * Content elements
- */
-$GLOBALS['TL_DCA']['tl_content']['palettes']['timeline_element'] = '{type_legend},type;{timeline_legend_content},timeline_year,text;{timeline_legend_settings},timeline_year_color_bg,timeline_year_color_text,timeline_lines_color_bg;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['timeline_element'] = '{type_legend},type;{timeline_legend_content},timeline_year,text;{gallery_legend},multiSRC,sortBy,metaIgnore,size,fullsize;{timeline_legend_settings},timeline_year_color_bg,timeline_year_color_text,timeline_lines_color_bg;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['timeline_year'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_year'],
@@ -34,6 +21,12 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['timeline_year_color_bg'] = [
     'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
     'sql'       => "char(7) NOT NULL default ''"
 ];
+$GLOBALS['TL_DCA']['tl_content']['fields']['timeline_lines_color_bg'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_lines_color_bg'],
+    'inputType' => 'text',
+    'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
+    'sql'       => "char(7) NOT NULL default ''"
+];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['timeline_lines_color_bg'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_lines_color_bg'],
@@ -41,3 +34,5 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['timeline_lines_color_bg'] = [
     'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
     'sql'       => "char(7) NOT NULL default ''"
 ];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['multiSRC']['eval']['isGallery'] = true;
--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,26 +1,13 @@
 <?php
 
-/*
- * This file is part of Timeline.
- * 
- * (c) Niels Hegmans 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-timeline-bundle
- */
-
-/**
- * Content element
- */
 $GLOBALS['TL_LANG']['CTE']['timeline_element'] = ['Timeline', 'Timeline'];
 
 $GLOBALS['TL_LANG']['tl_content']['timeline_year_color_bg'] = ['Background color of the year', 'Standard color is #d10000.'];
 $GLOBALS['TL_LANG']['tl_content']['timeline_year_color_text'] = ['Font color of the year', 'Standard color is #ffffff.'];
 $GLOBALS['TL_LANG']['tl_content']['timeline_lines_color_bg'] = ['Background color of the lines', 'Standard color is #989898.'];
 
-$GLOBALS['TL_LANG']['tl_content']['timeline_year'] = ['Year', 'This content is highlighted in color.'];
+$GLOBALS['TL_LANG']['tl_content']['timeline_year'] = ['Headline', 'This content is highlighted in color.'];
 
 $GLOBALS['TL_LANG']['tl_content']['timeline_legend_settings'] = 'Timeline-Settings';
 $GLOBALS['TL_LANG']['tl_content']['timeline_legend_content'] = 'Timeline-Content';
-
+$GLOBALS['TL_LANG']['tl_content']['gallery_legend'] = 'Gallery';
