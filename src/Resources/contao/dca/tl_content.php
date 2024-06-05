da276af89dbd4e85d08e82734067afea25904d6d
Niels <info@heimseiten.de>
2024-06-05 14:15:22 +0200

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,38 +1,27 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['timeline_element'] = '{type_legend},type;{timeline_legend_content},timeline_year,text;{gallery_legend},multiSRC,sortBy,metaIgnore,size,fullsize;{timeline_legend_settings},timeline_year_color_bg,timeline_year_color_text,timeline_lines_color_bg;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\DataContainer;
+
+PaletteManipulator::create()
+    ->addLegend('timeline_legend_content', 'type_legend', PaletteManipulator::POSITION_AFTER)
+    
+    ->addField('timeline_year', 'timeline_legend_content', PaletteManipulator::POSITION_APPEND)
+    ->addField('headline', 'timeline_legend_content', PaletteManipulator::POSITION_APPEND)
+    ->addField('text', 'timeline_legend_content', PaletteManipulator::POSITION_APPEND)
+    ->addField('multiSRC', 'timeline_legend_content', PaletteManipulator::POSITION_APPEND)
+    ->addField('size', 'timeline_legend_content', PaletteManipulator::POSITION_APPEND)
+    
+    ->applyToPalette('default', 'tl_content')
+;
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['timeline_year'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_year'],
     'inputType' => 'text',
-    'eval'      => ['mandatory' => true, 'tl_class' => 'clr'],
+    'eval'      => ['mandatory' => true, 'tl_class' => 'w50'],
     'sql'       => "char(255) NOT NULL default ''"
 ];
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['timeline_year_color_text'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_year_color_text'],
-    'inputType' => 'text',
-    'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
-    'sql'       => "char(7) NOT NULL default ''"
-];
-$GLOBALS['TL_DCA']['tl_content']['fields']['timeline_year_color_bg'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_year_color_bg'],
-    'inputType' => 'text',
-    'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
-    'sql'       => "char(7) NOT NULL default ''"
-];
-$GLOBALS['TL_DCA']['tl_content']['fields']['timeline_lines_color_bg'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_lines_color_bg'],
-    'inputType' => 'text',
-    'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
-    'sql'       => "char(7) NOT NULL default ''"
-];
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['timeline_lines_color_bg'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['timeline_lines_color_bg'],
-    'inputType' => 'text',
-    'eval'      => array('maxlength'=>6, 'colorpicker'=>true, 'isHexColor'=>true, 'decodeEntities'=>true, 'tl_class'=>'w50 wizard'),
-    'sql'       => "char(7) NOT NULL default ''"
-];
-
+$GLOBALS['TL_DCA']['tl_content']['fields']['headline']['eval']['tl_class'] = 'w50';
+$GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval']['tl_class'] = 'clr';
 $GLOBALS['TL_DCA']['tl_content']['fields']['multiSRC']['eval']['isGallery'] = true;
